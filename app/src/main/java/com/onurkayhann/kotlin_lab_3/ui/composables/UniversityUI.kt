b61f225cf0b67f3d2affcab39278cea6851a155c
Onur Kayhan <onur.kayhan@chasacademy.se>
2024-04-19 11:54:01 +0200

    add scroll using lazyColumns for the universities

--- app/src/main/java/com/onurkayhann/kotlin_lab_3/ui/composables/UniversityUI.kt
+++ app/src/main/java/com/onurkayhann/kotlin_lab_3/ui/composables/UniversityUI.kt
@@ -3,9 +3,13 @@ package com.onurkayhann.kotlin_lab_3.ui.composables
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -16,6 +20,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.ViewModel
 import com.onurkayhann.kotlin_lab_3.api.University
 import com.onurkayhann.kotlin_lab_3.api.UniversityRetrofit
@@ -49,13 +54,26 @@ fun UniversityUI(viewModel: UniversityViewModel = viewModel()) {
 
         val universities = viewModel.universityUiState.value
 
-        if (universities.isNotEmpty()) {
-            for (university in universities) {
-                Text(text = "Name: ${university.name}")
-                Text(text = "Country: ${university.country}")
+        // Display message if there are no universities or if country is not entered yet
+        if (universities.isEmpty() && country.isNotBlank()) {
+            Text(
+                text = "No universities found for the entered country.",
+                modifier = Modifier.padding(vertical = 8.dp),
+            )
+        } else if (country.isBlank()) {
+            Text(
+                text = "Enter the country name to see available universities",
+                modifier = Modifier.padding(vertical = 8.dp),
+            )
+        }
+
+        LazyColumn {
+            items(universities) { university ->
+                Column {
+                    Text(text = "Name: ${university.name}")
+                    Text(text = "Country: ${university.country}")
+                }
             }
-        } else {
-            Text(text = "Enter the country name to see available universities")
         }
     }
 }
