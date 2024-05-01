1630f5c588112f4a7bc344a1da8b53ab923a657d
Onur Kayhan <onur.kayhan@chasacademy.se>
2024-05-01 22:29:11 +0200

    install dependencies for Room DB

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,6 +54,15 @@ android {
 
 dependencies {
 
+    // Room DB
+    implementation("androidx.room:room-runtime:2.6.1")
+    androidTestImplementation("androidx.room:room-testing:2.6.1")
+
+    annotationProcessor("androidx.room:room-compiler:2.6.1")
+
+    // LifeCycle
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+
     // Retrofit
     implementation ("com.squareup.retrofit2:retrofit:2.9.0")
 
